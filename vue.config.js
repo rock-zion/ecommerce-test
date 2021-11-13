c6133f7e7c9dcea85f3bc4c106015fb35fea553f
rock-zion <rock_zion@outlook.com>
2021-11-13 23:19:45 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ecommerce-test-server.herokuapp.com/',
         changeOrigin: true,
         logLevel: 'debug',
         pathRewrite: { '^/api': '/' },
